dadadc67466c0693ff63a5e6d617fe1cd51298f6
tr1ptychs <97324991+tr1ptychs@users.noreply.github.com>
2024-08-09 16:44:24 -0500

    Update scrap.lua

--- lua/scrap/scrap.lua
+++ lua/scrap/scrap.lua
@@ -64,8 +64,7 @@ function M.toggle_pad()
     vim.api.nvim_buf_set_keymap(buf_id, 'n', '<Esc>',
       '<Cmd>lua require("scrap.scrap").toggle_pad()<CR>',
       { noremap = true, silent = true })
-    -- optional: set buffer options
-    -- vim.api.nvim_buf_set_option(buf_id, "bufhidden", "wipe")
+
   end
 end
 
